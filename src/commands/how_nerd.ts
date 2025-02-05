1274ff7752b895fa6547ca8b2068c5f71e20d2ec
Νεφέλη Κωνσταντάτου <nephelekonstantatou@gmail.com>
2025-02-05 19:32:58 +0200

    Made the how_nerd command be somewhat sensible and less extreme and off-putting.

--- src/commands/how_nerd.ts
+++ src/commands/how_nerd.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder, ChannelType } from 'discord.js'
 import * as command from '../command'
 
 function distributeMessage(message_list: Array<string>): string {
-    if(message_list.length === 0) return "";
+    if(message_list.length === 0) return "zero";
     return message_list[Math.floor(Math.random() * message_list.length)];
 }
 
@@ -13,13 +13,12 @@ interface NerdData {
 const HowNerd : command.Command = {
     data: new SlashCommandBuilder()
         .setName('how_nerd')
-		.setDescription('You = not a nerd? hmmmmmm'),
+		.setDescription('Do you know how nerdy you really are? Because I do.'),
     async execute(interaction, client) {
         const percentage = Math.random() * 100;
         let message: string = `You are ${percentage.toFixed(1)}% nerdy. `;
         
         const nerd_data: NerdData = command.loadData(this);
-
         if(percentage < 10)
             message += distributeMessage(nerd_data.messages[0]);
         else if(percentage < 20)
@@ -31,8 +30,8 @@ const HowNerd : command.Command = {
         else if(percentage < 80)
             message += distributeMessage(nerd_data.messages[4]);
         else if(percentage < 90)
-            distributeMessage(nerd_data.messages[5]);
-        else distributeMessage(nerd_data.messages[6]); 
+            message += distributeMessage(nerd_data.messages[5]);
+        else message += distributeMessage(nerd_data.messages[6]); 
     
         interaction.reply(message);
     }
